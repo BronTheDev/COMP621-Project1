1fc125764438436d69fbd2fe1cc8f33648961092
Geterry Sidbury <gksidburycrawford@aggies.ncat.edu>
2020-02-05 09:52:40 -0500

    First Push

--- eclipse-workspace/jdbcdemo/src/jdbcdemo/Driver.java
+++ eclipse-workspace/jdbcdemo/src/jdbcdemo/Driver.java
@@ -8,15 +8,20 @@ public class Driver {
 		// TODO Auto-generated method stub
 		try {
 			// 1. Get a connection
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost;integratedSecurity=true");
+			Connection myConn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433");
+		
 
 			// 2. Create a statement
 			Statement myStmt = myConn.createStatement();
 			
 			
 			// 3. Execute SQL query
-			ResultSet myRs = myStmt.executeQuery("select ")
+			ResultSet myRs = myStmt.executeQuery("select * from patients");
 			// 4. Process the result set
+			while (myRs.next()) {
+				System.out.print(myRs.getString("name") + myRs.getString("address") + myRs.getString("ID") + myRs.getString("phone_number") + myRs.getString("account_balance"));
+				
+			}
 			
 		} catch (Exception exc) {
 			exc.printStackTrace();
