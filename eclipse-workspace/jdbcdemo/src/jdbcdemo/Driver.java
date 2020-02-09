d55746c252f6b6bb0f7e14d23e4eae4c3a9fe4be
Geterry Sidbury <gksidburycrawford@aggies.ncat.edu>
2020-02-09 15:56:27 -0500

    Added two methods; make_patient and show_patients.

--- eclipse-workspace/jdbcdemo/src/jdbcdemo/Driver.java
+++ eclipse-workspace/jdbcdemo/src/jdbcdemo/Driver.java
@@ -8,30 +8,77 @@ public class Driver {
 		// TODO Auto-generated method stub
 		try {
 			// 1. Get a connection
-			String ConnectionUrl = "jdbc:sqlserver://localhost;databaseName=Hospital;integratedSecurity=true";
+			String user_name = "Doctor_Sidbury";
+			String password = "Doctor";
+			String ConnectionUrl = "jdbc:sqlserver://localhost;databaseName=Hospital;user=" + user_name + ";password=" + password;
 			Connection myConn = DriverManager.getConnection(ConnectionUrl);
 		
-
-			// 2. Create a statement
-			Statement myStmt = myConn.createStatement();
-			
-			
-			// 3. Execute SQL query
-			ResultSet myRs = myStmt.executeQuery("select * from patients");
-			
-			// 4. Process the result set
-			while (myRs.next()) {
-				System.out.print(myRs.getString("name") +  ", " + 
-				myRs.getString("address") + ", " + 
-				myRs.getString("ID") + ", " +
-				myRs.getString("phone_number") + ", " +
-				myRs.getString("account_balance"));
-				
-			}
-			
+			//make_patient("Redd Foreman", "202 Rainy St", "10836", "862-760-3329", "88", myConn);
+			show_patients(myConn);
 		} catch (Exception exc) {
 			exc.printStackTrace();
 		}
 	}
+
+	
+	public static void make_patient(String new_patient_name, String new_patient_address, 
+			String new_patient_ID, String new_patient_phone, String new_patient_balance,
+			Connection myConn) {
+		
+		try {
+			
+			// 2. Create a statement
+			Statement myStmt = myConn.createStatement();
+			
+			new_patient_name = "'" + new_patient_name + "', ";
+			new_patient_address = "'" + new_patient_address + "', ";
+			new_patient_ID = "'" + new_patient_ID + "', ";
+			new_patient_phone = "'" + new_patient_phone + "', ";
+			new_patient_balance = "'" + new_patient_balance + "'";
+			
+			String new_patient_stmt = "insert into patients " +
+									" (name, address, ID, phone_number, account_balance)" +
+									" values (" + new_patient_name + new_patient_address + new_patient_ID + new_patient_phone + new_patient_balance + ")";
+			
+			
+			// 3. Execute SQL query
+			myStmt.executeUpdate(new_patient_stmt);
+			
+			// 4. Process the result set
+			System.out.println("Patient created.");
+			
+			} catch (Exception exc) {
+				System.out.println("Could not execute command. Please check input.");
+				exc.printStackTrace();
+			}
+		
+	}
+	
+	public static void show_patients(Connection myConn) {
+		try {
+			
+			// 2. Create a statement
+			Statement myStmt = myConn.createStatement();
+			
+			// 3. Execute SQL query
+			ResultSet Show_Patients = myStmt.executeQuery("select * from patients");
+			
+			
+			// 4. Process the result set
+			while (Show_Patients.next()) {
+				System.out.println(Show_Patients.getString("name") +  ", " + 
+				Show_Patients.getString("address") + ", " + 
+				Show_Patients.getString("ID") + ", " +
+				Show_Patients.getString("phone_number") + ", " +
+				Show_Patients.getInt("account_balance"));
+			
+			}
+		} catch (Exception exc) {
+			System.out.println("Could not execute command. Please check input.");
+			exc.printStackTrace();
+		}
+			
+	}
+	
 
 }
