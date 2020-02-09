b4460cc48937f0f040e0581e5f7adfbec113adc9
Geterry Sidbury <gksidburycrawford@aggies.ncat.edu>
2020-02-09 18:58:33 -0500

    Added Login Parameters from User input and created check_role method.

--- eclipse-workspace/jdbcdemo/src/jdbcdemo/Driver.java
+++ eclipse-workspace/jdbcdemo/src/jdbcdemo/Driver.java
@@ -1,26 +1,70 @@
 package jdbcdemo;
 
 import java.sql.*;
-
+import java.util.Scanner;
 public class Driver {
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
 			// 1. Get a connection
+			Scanner user_input = new Scanner(System.in);
+			
+			
+			System.out.println("Please enter your Username, Password, and Role");
+			System.out.print("Username: ");
+		//	String user_name = user_input.next(); 
 			String user_name = "Doctor_Sidbury";
+			
+			System.out.print("Password: ");
+		//	String password = user_input.next(); //"Doctor";
 			String password = "Doctor";
+			
+			System.out.print("Role: ");
+		//	String user_role = user_input.next(); //"Doctor";
+			String user_role = "Doctor";
+			
 			String ConnectionUrl = "jdbc:sqlserver://localhost;databaseName=Hospital;user=" + user_name + ";password=" + password;
 			Connection myConn = DriverManager.getConnection(ConnectionUrl);
 		
 			//make_patient("Redd Foreman", "202 Rainy St", "10836", "862-760-3329", "88", myConn);
+			
+			check_role(user_name, password, user_role, myConn);
+			
 			show_patients(myConn);
 		} catch (Exception exc) {
 			exc.printStackTrace();
 		}
 	}
 
+	public static void check_role(String user_name, String password,String user_role, Connection myConn) {
+		try {
+		
+			PreparedStatement myStmt = myConn.prepareStatement("select * from users where user_name=? and password=?");
+			myStmt.setString(1, user_name);
+			myStmt.setString(2, password);
+			ResultSet rs = myStmt.executeQuery();
+			if (rs.next()) {           
+			    String roles =  rs.getString("role");
+			    roles = roles.replaceAll("\\s", "");
+			    
+			    if (roles.equals(user_role)) {
 	
+			    	System.out.println("cool");
+			    } else {
+			    	
+			    	System.out.println(user_role + " vs " + roles + "m");
+			    }
+			    
+			}
+		} catch (Exception exc) {
+			System.out.println("Could not execute command. Please check input.");
+			exc.printStackTrace();
+		}
+		
+	}
 	public static void make_patient(String new_patient_name, String new_patient_address, 
 			String new_patient_ID, String new_patient_phone, String new_patient_balance,
 			Connection myConn) {
