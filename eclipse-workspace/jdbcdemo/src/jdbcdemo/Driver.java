b5b33a36391072048416f6b9f0adfa1c57cd4ce3
Geterry Sidbury <gksidburycrawford@aggies.ncat.edu>
2020-02-10 16:06:53 -0500

    Added support for remote connections to SQL Server and Database.

--- eclipse-workspace/jdbcdemo/src/jdbcdemo/Driver.java
+++ eclipse-workspace/jdbcdemo/src/jdbcdemo/Driver.java
@@ -14,32 +14,54 @@ public class Driver {
 			
 			
 			System.out.println("Please enter your Username, Password, and Role");
+			
 			System.out.print("Username: ");
-		//	String user_name = user_input.next(); 
-			String user_name = "Doctor_Sidbury";
+			String user_name = user_input.next(); 
 			
 			System.out.print("Password: ");
-		//	String password = user_input.next(); //"Doctor";
-			String password = "Doctor";
+			String password = user_input.next();
 			
 			System.out.print("Role: ");
-		//	String user_role = user_input.next(); //"Doctor";
-			String user_role = "Doctor";
+			String user_role = user_input.next();
 			
-			String ConnectionUrl = "jdbc:sqlserver://localhost;databaseName=Hospital;user=" + user_name + ";password=" + password;
+			String ConnectionUrl = "jdbc:sqlserver://DESKTOP-GMPS9UK;databaseName=Hospital;user=" + user_name + ";password=" + password;
 			Connection myConn = DriverManager.getConnection(ConnectionUrl);
 		
 			//make_patient("Redd Foreman", "202 Rainy St", "10836", "862-760-3329", "88", myConn);
 			
-			check_role(user_name, password, user_role, myConn);
+			boolean role_check = check_role(user_name, password, user_role, myConn);
 			
-			show_patients(myConn);
+			if (role_check) {
+				show_patients(myConn);
+				if (user_role.equals("Nurse")) {
+					System.out.println("Would you like to make a patient? Please type \"Yes\"or \"No\" ");
+					String responce = user_input.next();
+					if (responce.equals("Yes")) {
+						System.out.print("Please enter the Patient's Name: ");
+						String name = user_input.next();
+						System.out.print("Please enter the Patient's Address: ");
+						String address = user_input.next();
+						System.out.print("Please enter the Patients ID: ");
+						String ID = user_input.next();
+						System.out.print("Please enter the Patients Phone-Number: ");
+						String phone = user_input.next();
+						System.out.print("Please enter the Patients Current Balance: ");
+						String balance = user_input.next();
+						make_patient(name, address, ID, phone, balance, myConn);
+						
+					}
+				}
+				
+			} else {
+				System.out.println("Acess has been denied for this user. Please check credentials in system input");
+			}
+			
 		} catch (Exception exc) {
 			exc.printStackTrace();
 		}
 	}
 
-	public static void check_role(String user_name, String password,String user_role, Connection myConn) {
+	public static boolean check_role(String user_name, String password,String user_role, Connection myConn) {
 		try {
 		
 			PreparedStatement myStmt = myConn.prepareStatement("select * from users where user_name=? and password=?");
@@ -51,18 +73,20 @@ public class Driver {
 			    roles = roles.replaceAll("\\s", "");
 			    
 			    if (roles.equals(user_role)) {
-	
-			    	System.out.println("cool");
+			    	System.out.println("This user is confirmed to be a " + roles);
+			    	return true;
 			    } else {
-			    	
-			    	System.out.println(user_role + " vs " + roles + "m");
+			    	System.out.println("This user is confirmed to NOT be a " + roles);
+			    	return false;
 			    }
 			    
 			}
+			return false;
 		} catch (Exception exc) {
 			System.out.println("Could not execute command. Please check input.");
 			exc.printStackTrace();
 		}
+		return false;
 		
 	}
 	public static void make_patient(String new_patient_name, String new_patient_address, 
